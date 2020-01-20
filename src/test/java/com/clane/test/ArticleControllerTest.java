a3db8d01ba61a62caa3fd37ba36aae14bc88a9d6
Asukwo Enenim <Asukwo.Enenim@etranzact.com>
2020-01-20 22:16:50 +0100

    Added postman collection

--- src/test/java/com/clane/test/ArticleControllerTest.java
+++ src/test/java/com/clane/test/ArticleControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
